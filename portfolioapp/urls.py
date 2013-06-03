9cd65cb4691df734f15cb702f8361ae2fe9e7cd4
mel <mel@mel-thinkpad>
2013-06-03 15:09:46 -0400

    don't show project category posts anywhere in the blog menus

--- portfolioapp/urls.py
+++ portfolioapp/urls.py
@@ -16,14 +16,30 @@ urlpatterns = patterns("mezzanine.blog.views",
         "blog_post_feed", name="blog_post_feed"),
     url("^%stag/(?P<tag>.*)/feeds/(?P<format>.*)%s$" % _slashes,
         "blog_post_feed", name="blog_post_feed_tag"),
+)
+
+urlpatterns += patterns("portfolioapp.views",
     url("^%stag/(?P<tag>.*)%s$" % _slashes, "blog_post_list",
         name="blog_post_list_tag"),
+        
+)
+
+urlpatterns += patterns("mezzanine.blog.views",
     url("^%scategory/(?P<category>.*)/feeds/(?P<format>.*)%s$" % _slashes,
         "blog_post_feed", name="blog_post_feed_category"),
+)
+
+urlpatterns += patterns("portfolioapp.views",     
     url("^%scategory/(?P<category>.*)%s$" % _slashes,
         "blog_post_list", name="blog_post_list_category"),
+)
+
+urlpatterns += patterns("mezzanine.blog.views",
     url("^%sauthor/(?P<username>.*)/feeds/(?P<format>.*)%s$" % _slashes,
         "blog_post_feed", name="blog_post_feed_author"),
+)
+
+urlpatterns += patterns("portfolioapp.views",
     url("^%sauthor/(?P<username>.*)%s$" % _slashes,
         "blog_post_list", name="blog_post_list_author"),
     url("^%sarchive/(?P<year>\d{4})/(?P<month>\d{1,2})%s$" % _slashes,
@@ -37,15 +53,10 @@ urlpatterns = patterns("mezzanine.blog.views",
         "blog_post_detail", name="blog_post_detail_month"),
     url("^%s(?P<year>\d{4})/(?P<slug>.*)%s$" % _slashes,
         "blog_post_detail", name="blog_post_detail_year"),
-)
-
-urlpatterns += patterns("portfolioapp.views",
     url("^%s(?P<slug>.*)%s$" % _slashes, "blog_post_detail",
         name="blog_post_detail"),
-)
-
-urlpatterns += patterns("mezzanine.blog.views",
     url("^%s$" % _slashes[1], "blog_post_list", name="blog_post_list"),
 )
+
                         
                         
--- portfolioapp/views.py
+++ portfolioapp/views.py
@@ -3,6 +3,69 @@ from django.shortcuts import get_object_or_404
 from mezzanine.blog.models import BlogPost, BlogCategory
 from mezzanine.utils.views import render
 
+from calendar import month_name
+
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+
+from mezzanine.blog.models import BlogPost, BlogCategory
+from mezzanine.conf import settings
+from mezzanine.generic.models import Keyword
+from mezzanine.utils.views import render, paginate
+from mezzanine.utils.models import get_user_model
+
+User = get_user_model()
+
+
+def blog_post_list(request, tag=None, year=None, month=None, username=None,
+                   category=None, template="blog/blog_post_list.html"):
+    """
+    Display a list of blog posts that are filtered by tag, year, month,
+    author or category. Custom templates are checked for using the name
+    ``blog/blog_post_list_XXX.html`` where ``XXX`` is either the
+    category slug or author's username if given. Excludes posts in category "projects"
+    """
+    settings.use_editable()
+    templates = []
+    
+    blog_posts = BlogPost.objects.published(for_user=request.user)
+    
+    if (not category) or (not category.upper() == "projects".upper()):
+        project_category = BlogCategory.objects.filter(title__iexact="projects")
+        if project_category.exists():
+            blog_posts = blog_posts.exclude(categories__in=project_category)
+        
+    if tag is not None:
+        tag = get_object_or_404(Keyword, slug=tag)
+        blog_posts = blog_posts.filter(keywords__in=tag.assignments.all())
+    if year is not None:
+        blog_posts = blog_posts.filter(publish_date__year=year)
+        if month is not None:
+            blog_posts = blog_posts.filter(publish_date__month=month)
+            month = month_name[int(month)]
+    if category is not None:
+        category = get_object_or_404(BlogCategory, slug=category)
+        blog_posts = blog_posts.filter(categories=category)
+        templates.append(u"blog/blog_post_list_%s.html" %
+                          unicode(category.slug))
+    author = None
+    if username is not None:
+        author = get_object_or_404(User, username=username)
+        blog_posts = blog_posts.filter(user=author)
+        templates.append(u"blog/blog_post_list_%s.html" % username)
+
+    prefetch = ("categories", "keywords__keyword")
+    blog_posts = blog_posts.select_related("user").prefetch_related(*prefetch)
+    blog_posts = paginate(blog_posts, request.GET.get("page", 1),
+                          settings.BLOG_POST_PER_PAGE,
+                          settings.MAX_PAGING_LINKS)
+    context = {"blog_posts": blog_posts, "year": year, "month": month,
+               "tag": tag, "category": category, "author": author}
+    templates.append(template)
+    return render(request, templates, context)
+
+
+
 def blog_post_detail(request, slug, year=None, month=None, day=None,
                      template="blog/blog_post_detail.html"):
     """. Custom templates are checked for first by category (like the blog post list page), then 
