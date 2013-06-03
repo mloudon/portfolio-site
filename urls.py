4949db921b429d86ade1c0e592aaae8a4f087acf
mel <mel@mel-thinkpad>
2013-06-02 21:28:35 -0400

    custom blog post detail view

--- urls.py
+++ urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from mezzanine.core.views import direct_to_template
 
 
+
 admin.autodiscover()
 
 # Add the urlpatterns for any custom Django applications here.
@@ -27,6 +28,7 @@ urlpatterns = patterns("",
     # one homepage pattern, so if you use a different one, comment this
     # one out.
 
+    url("^$", direct_to_template, {"template": "index.html"}, name="home"),
     url("^$", direct_to_template, {"template": "index.html"}, name="home"),
 
     # HOMEPAGE AS AN EDITABLE PAGE IN THE PAGE TREE
