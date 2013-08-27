95336dce7f51396731f9ac52c1ff59db38dad39c
mel <mel@mel-thinkpad>
2013-08-27 10:54:17 -0400

    log debug

--- locationlogger/views.py
+++ locationlogger/views.py
@@ -16,12 +16,14 @@ def update_location(request):
             logging.error("Raw Data: %s",request.body)
             try:
                 loc_data = json.loads(request.body)
+                logging.error("json loaded")
                 utc_date = datetime.datetime.fromtimestamp(float(loc_data["timestamp"])//1000.0).replace(tzinfo=utc)
+                logging.error("utc date %s", utc_date)
                 loc = Location.objects.create(lat=float(loc_data["lat"]), lon=float(loc_data["lon"]), 
                                               accuracy=float(loc_data["acc"]), 
                                               update_time=utc_date)
                 loc.save()
-                logging.debug("saved location %s", loc)
+                logging.debug("saved location")
                 return HttpResponse(status=201)
             except:
                 logging.error("exception saving location, error was %s", sys.exc_info()[0])
