2ecbd1b7aa6feeaade5cb8ca52029c9bf0a9eb87
mel <mel@mel-thinkpad>
2013-08-27 20:09:57 -0400

    float to decimal

--- locationlogger/models.py
+++ locationlogger/models.py
@@ -3,6 +3,18 @@ from pygeocoder import Geocoder
 from decimal import *
 
 
+def float_to_decimal(f):
+    "Convert a floating point number to a Decimal with no loss of information"
+    n, d = f.as_integer_ratio()
+    numerator, denominator = Decimal(n), Decimal(d)
+    ctx = Context(prec=60)
+    result = ctx.divide(numerator, denominator)
+    while ctx.flags[Inexact]:
+        ctx.flags[Inexact] = False
+        ctx.prec *= 2
+        result = ctx.divide(numerator, denominator)
+    return result
+
 class Location(models.Model):
     lat = models.DecimalField('latitude',decimal_places=6,max_digits=8)
     lon = models.DecimalField('longitude',decimal_places=6,max_digits=9)
@@ -24,11 +36,12 @@ class Location(models.Model):
             latlon = Geocoder.geocode(city).coordinates
             print latlon
         
-        self.city_lat = latlon[0]
-        self.city_lon = latlon[1]
+        self.city_lat = float_to_decimal(latlon[0])
+        self.city_lon = float_to_decimal(latlon[1])
         self.city_str = city
         
         super(Location, self).save(*args, **kwargs)
+        
     
     class Meta:
         get_latest_by = "update_time"
